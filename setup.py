95b1f215342a434bec0b5e5b04f7f02c8ebb51a9
yoyota <smarthorse6@gmail.com>
2020-02-08 18:11:02 +0900

    Fix pylint warnings

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-required_packages = ["pillow", "pytest", "requests"]
+required_packages = ["pillow", "pylint", "pytest", "requests"]
 
 setup(
     name="pi_camera_capture",
