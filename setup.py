d3c08c864ee10b73dfa139b12dba2f06262ba56e
yoyota <smarthorse6@gmail.com>
2020-02-10 19:16:18 +0900

    Add README.md

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
+from os import path
 from setuptools import setup, find_packages
 
 DEPENDENCIES = ["fire", "requests"]
 TEST_DEPENDENCIES = ["pillow", "pylint", "pytest", "responses"]
 
+
+def get_long_description():
+    workspace = path.abspath(path.dirname(__file__))
+    with open(path.join(workspace, "README.md"), encoding="utf-8") as readme:
+        return readme.read()
+
+
 setup(
     name="pi-camera-capture",
     version="0.1.1",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
     packages=find_packages(exclude=("tests",)),
     entry_points={
         "console_scripts": ["pi-camera-capture=pi_camera_capture.cli:cli"]
